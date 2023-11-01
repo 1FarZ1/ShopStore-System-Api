82809e5e82ed1ff2ca1e826b43184b4497c728f3
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-11-01 19:02:18 +0100

    adding orders layers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { databaseProviders } from './common/db.providers';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ProductModule, AuthModule],
+  imports: [ProductModule, AuthModule, OrderModule],
   controllers: [],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
