e86dd4e69d85d7b02cf63a407757ffc915986d04
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-11-02 01:44:44 +0100

    adding some comments for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './product/product.module';
-import { AuthModule } from './auth/auth.module';
-import { databaseProviders } from './common/db.providers';
-import { OrderModule } from './order/order.module';
+// import { ProductModule } from './product/product.module';
+// import { AuthModule } from './auth/auth.module';
+// import { databaseProviders } from './common/db.providers';
+// import { OrderModule } from './order/order.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [ProductModule, AuthModule, OrderModule],
-  controllers: [],
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  // imports: [ProductModule, AuthModule, OrderModule],
+  controllers: [AppController],
+  // providers: [...databaseProviders],
+  // exports: [...databaseProviders],
 })
 export class AppModule {}
