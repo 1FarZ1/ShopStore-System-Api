45ef735ebf42b8a7a42a5196be1146982fcd9b19
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-12-07 02:43:22 +0100

    adding product entity

--- src/reports/entities/report.entity.ts
+++ src/reports/entities/report.entity.ts
@@ -1,1 +1,21 @@
-export class Report {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'reports',
+})
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  productId: number;
+
+  @Column()
+  comment: string;
+
+  @Column()
+  rate: number;
+}
