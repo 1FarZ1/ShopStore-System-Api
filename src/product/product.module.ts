912f0ad8d9746f74dba3d9bb99b4d66accc81683
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-12-08 18:37:09 +0100

    fixing bug

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,11 +2,13 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { UsersService } from 'src/users/users.service';
 @Module({
   imports: [
-    
+      
   ],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService,
+    UsersService],
 })
 export class ProductModule {}
